9a65478dd66ba2330b712248bcb181a42832af56
j3553pat <jessecodedev@gmail.com>
2022-11-11 23:06:58 -0600

    added js and html file for contact page. Will create new page for third party contact.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-document.getElementById("submit").addEventListener("click", submitBtn);
+const infopage = document.getElementById("info")
 
-function submitBtn() {
+new URLSearchParams(window.location.search).forEach((value, name) => {
+    infopage.append(`${name} : ${value}`)
+    infopage.append(document.createElement("br"))
+  })
+
+document.getElementById("submit").addEventListener("click", sendBtn);
+
+function sendBtn() {
     alert("Thank you for your time and we will reach out to you soon!")
 }
